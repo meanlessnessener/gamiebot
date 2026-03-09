4e60ed2cf0f6012418c50d06a2afd400f31e8aee
Yamca <asamalin2004@gmail.com>
2026-03-10 00:54:14 +0500

    Session meneger

--- src/main/java/GamieBot/presenter/requestHandling/game/SessionManager.java
+++ src/main/java/GamieBot/presenter/requestHandling/game/SessionManager.java
@@ -1,16 +1,20 @@
 package GamieBot.presenter.requestHandling.game;
 
+import java.util.HashMap;
+
 import GamieBot.model.GameSession;
+import GamieBot.model.users.User;
 import GamieBot.model.users.UserManager;
-import java.util.ArrayList;
 
 
 public class SessionManager {
-    private final ArrayList<GameSession> sessions;
+    private final HashMap<User, GameSession> sessions;
     private final UserManager users;
     
     public SessionManager(UserManager users) {
-        sessions = new ArrayList<>();
+        sessions = new HashMap<>();
         this.users = users;
     }
+
+
 }
